54c4965b4e042e60ee603baab160e4b8d98a2eaf
MFM_Fazrin <mmfazrin@phcc.gov.qa>
2022-07-04 12:24:20 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace GQLPlayGround
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime = DateTime.UtcNow.ToLocalTime();
+
+            DateTime currentDate = DateTime.Now.ToUniversalTime().ToLocalTime();
+            Console.WriteLine(currentDateTime);
+            Console.WriteLine(currentDateTime.Kind);
+
+            Console.WriteLine(currentDate);
+            Console.WriteLine(currentDate.Kind);
+
+            Console.ReadLine();
+        }
+    }
+}
